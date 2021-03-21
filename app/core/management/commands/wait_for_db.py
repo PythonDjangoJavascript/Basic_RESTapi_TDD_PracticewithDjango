41cfe7d3309a6987fcc6eb34b2926a6c05b5779a
Nuruddinsayeed <nuruddinsayeed@gmail.com>
2021-03-21 22:58:49 +0600

    Bug in command fixed

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
 
         while not db_conn:
             try:
-                db_conn = connections["defauld"]
+                db_conn = connections["default"]
             except OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
